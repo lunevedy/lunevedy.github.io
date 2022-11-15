49bc3501879186536c68084681e738a0c03ca749
lunevedy <115201583+lunevedy@users.noreply.github.com>
2022-11-15 08:38:37 +0000

    Add files via upload

--- ui/assets/js/ui-global.js
+++ ui/assets/js/ui-global.js
@@ -46,20 +46,20 @@ divs.forEach(el => el.addEventListener('click', event => {
     // Unhide current menu
     elItem_show.classList.remove("dropdown-hidden");
 
-    if (iframe.contentWindow.document.querySelector('.col-1') ) {
+    // if (iframe.contentWindow.document.querySelector('.col-1') ) {
         // Work with upper block
-        if (menuId==="2") {
-            window.scrollTo(0,0);
-        }
+        // if (menuId==="2") {
+        //     window.scrollTo(0,0);
+        // }
         // Work with columns
-        else if (menuId==="3") {
-            iframe.contentWindow.document.querySelector("div[class^='flex-cols-']").scrollIntoView(true);
-        }
+        // else if (menuId==="3") {
+        //     iframe.contentWindow.document.querySelector("div[class^='flex-cols-']").scrollIntoView(true);
+        // }
         // Work with buttons
-        else if (menuId==="4") {
-            window.scrollTo(0,document.body.scrollHeight);
-        }
-    }
+        // else if (menuId==="4") {
+        //     window.scrollTo(0,document.body.scrollHeight);
+        // }
+    // }
     // Hide color picker
     hideSidebar();
 }));
